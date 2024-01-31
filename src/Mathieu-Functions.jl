21d3e1f601b66f937a5225d299d50128c26c1754
unknown <mahdi.sasar@gmail.com>
2024-01-31 13:43:16 +0100

    Fixed the normalization constant and the accuracy goal.

--- src/Mathieu-Functions.jl
+++ src/Mathieu-Functions.jl
@@ -9,12 +9,12 @@ function ce(m::Int64, q::Float64, x::AbstractVector{Float64})
         i = 8
         #Normalization as each new coefficient is computed:
         N = 2*A[2]^2 + sum(A[4:end].^2)
-        A = A ./N
-        while (abs(A[i-2]/A[2])>accuracy)
+        A = A ./sqrt(N)
+        while (abs(A[i-2])>accuracy)
             push!(A, ((a-(i-4.)*(i-4.))/q)*A[i-2]- A[i-4])
             push!(A, 0.)
             N = 2*A[2]^2 + sum(A[4:end].^2)
-            A = A ./N
+            A = A ./sqrt(N)
             i += 2
         end
         t = zeros(length(x))
@@ -29,12 +29,12 @@ function ce(m::Int64, q::Float64, x::AbstractVector{Float64})
         i = 9
         #Normalization as each new coefficient is computed:
         N = sum(A[1:end].^2)
-        A = A ./N
-        while (abs(A[i-2]/A[1])>accuracy)
+        A = A ./sqrt(N)
+        while (abs(A[i-2])>accuracy)
             push!(A, ((a-(i-2)*(i-2))/q)*A[i-2] - A[i-4])
             push!(A, 0.)
             N = sum(A[1:end].^2)
-            A = A ./N
+            A = A ./sqrt(N)
             i += 2
         end
         t = zeros(length(x))
@@ -54,12 +54,12 @@ function se(m::Int64, q::Float64, x::AbstractVector{Float64})
         i = 6
         #Normalization as each new coefficient is computed:
         N = sum(B[2:end].^2)
-        B = B ./N
-        while (abs(B[i-2]/B[2])>accuracy)
+        B = B ./sqrt(N)
+        while (abs(B[i-2])>accuracy)
             push!(B, ((a-(i-2.)*(i-2.))/q)*B[i-2]- B[i-4])
             push!(B, 0.)
             N = sum(B[2:end].^2)
-            B = B ./N
+            B = B ./sqrt(N)
             i += 2
         end
         t = zeros(length(x))
@@ -74,12 +74,12 @@ function se(m::Int64, q::Float64, x::AbstractVector{Float64})
         i = 5
         #Normalization as each new coefficient is computed:
         N = sum(B[1:end].^2)
-        B = B ./N
-        while (abs(B[i-2]/B[1])>accuracy)
+        B = B ./sqrt(N)
+        while (abs(B[i-2])>accuracy)
             push!(B, ((a-(i-2)*(i-2))/q)*B[i-2] - B[i-4])
             push!(B, 0.)
             N = sum(B[1:end].^2)
-            B = B ./N
+            B = B ./sqrt(N)
             i += 2
         end
         println(B)
